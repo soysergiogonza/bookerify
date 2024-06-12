7e498bc01b446ccbde149047402b2be4b57a94de
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2024-06-12 16:16:20 -0500

    remove: Deleted files on assets folder

--- public/assets/fonts/index.ts
+++
@@ -1,9 +0,0 @@
-import localFont from 'next/font/local'
-
-export const MonospaceKrypton = localFont({
-  src: './MonospaceKrypton/MonospaceKrypton-Bold.otf',
-})
-
-export const Inter = localFont({
-  src: './Inter/Inter-Regular.ttf',
-})
